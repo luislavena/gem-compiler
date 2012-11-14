3870937274080eb429cd520b5fcf6135f817109b
Luis Lavena <luislavena@gmail.com>
2012-11-14 18:25:06 -0300

    Modularize actions
    This aims to split #compile into smaller, easier to digest chunks of
    code.

--- lib/rubygems/compiler.rb
+++ lib/rubygems/compiler.rb
@@ -10,34 +10,15 @@ class Gem::Compiler
   # raise when there is a error
   class CompilerError < Gem::InstallError; end
 
+  attr_reader :tmp_dir, :target_dir
+
   def initialize(gemfile, output_dir)
     @gemfile    = gemfile
     @output_dir = output_dir
   end
 
   def compile
-    installer = Gem::Installer.new(@gemfile, :unpack => true)
-
-    # Hmm, gem already compiled?
-    if installer.spec.platform != Gem::Platform::RUBY
-      raise CompilerError,
-            "The gem file seems to be compiled already."
-    end
-
-    # Hmm, no extensions?
-    if installer.spec.extensions.empty?
-      raise CompilerError,
-            "There are no extensions to build on this gem file."
-    end
-
-    tmpdir     = Dir.mktmpdir
-    basename   = File.basename(@gemfile, '.gem')
-    target_dir = File.join(tmpdir, basename)
-
-    # unpack gem sources into target_dir
-    # We need the basename to keep the unpack happy
-    info "Unpacking gem: '#{basename}' in temporary directory..."
-    installer.unpack(target_dir)
+    unpack
 
     # build extensions
     installer.build_extensions
@@ -82,8 +63,7 @@ class Gem::Compiler
     # move the built gem to the original output directory
     FileUtils.mv File.join(target_dir, output_gem), @output_dir
 
-    # cleanup
-    FileUtils.rm_rf tmpdir
+    cleanup
 
     # return the path of the gem
     output_gem
@@ -97,5 +77,43 @@ class Gem::Compiler
 
   def debug(msg)
     say msg if Gem.configuration.really_verbose
+  end
+
+  def installer
+    return @installer if @installer
+
+    @installer = Gem::Installer.new(@gemfile, :unpack => true)
+
+    # Hmm, gem already compiled?
+    if @installer.spec.platform != Gem::Platform::RUBY
+      raise CompilerError,
+            "The gem file seems to be compiled already."
+    end
+
+    # Hmm, no extensions?
+    if @installer.spec.extensions.empty?
+      raise CompilerError,
+            "There are no extensions to build on this gem file."
+    end
+
+    @installer
+  end
+
+  def tmp_dir
+    @tmp_dir ||= Dir.mktmpdir
+  end
+
+  def unpack
+    basename    = File.basename(@gemfile, '.gem')
+    @target_dir = File.join(tmp_dir, basename)
+
+    # unpack gem sources into target_dir
+    # We need the basename to keep the unpack happy
+    info "Unpacking gem: '#{basename}' in temporary directory..."
+    installer.unpack(@target_dir)
+  end
+
+  def cleanup
+    FileUtils.rm_rf tmp_dir
   end
 end
